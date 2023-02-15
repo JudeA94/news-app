f1eb25945fee3828adcc252a81f1b7f50e81bc7d
Jude Ashworth <113235574+JudeA94@users.noreply.github.com>
2023-02-15 10:59:08 +0000

    Update newsClient.js

--- newsClient.js
+++ newsClient.js
@@ -1,7 +1,6 @@
+const apiKey = require("./apiKey");
+
 class NewsClient {
-  constructor() {
-    this.apiKey = process.env.API_KEY;
-  }
   getNewsData(search='', callback) {
     const url = `https://content.guardianapis.com/search?q=${search}&query-fields=headline&show-fields=thumbnail,headline,byline,standfirst&order-by=newest&api-key=${this.apiKey}`;
     return fetch(url)
